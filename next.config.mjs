3f61592a989e2d794f5206ace5b256fad36e542d
Snarloff <snarloff@gmail.com>
2024-02-03 22:55:55 -0300

    style: correção de espaçamento da next.config

--- next.config.mjs
+++ next.config.mjs
@@ -11,33 +11,32 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
           {
-            key: "X-Frame-Options",
-            value: "DENY",
+            key: 'X-Frame-Options',
+            value: 'DENY',
           },
           {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; image-src 'self'; script-src 'self' 'unsafe-eval'; font-src 'self'",
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; image-src 'self'; script-src 'self' 'unsafe-eval'; font-src 'self'",
           },
           {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
           },
           {
-            key: "Permissions-Policy",
-            value: "camera=(); battery=(self); geolocation=(); microphone=()",
+            key: 'Permissions-Policy',
+            value: 'camera=(); battery=(self); geolocation=(); microphone=()',
           },
           {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
           },
         ],
       },
-    ];
+    ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
